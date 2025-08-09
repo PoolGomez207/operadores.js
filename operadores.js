36e06d544bb17f37695aa29f92201e1cd279af8e
PoolGomez2007 <poolgomez27@gmail.com>
2025-08-09 09:37:52 -0500

    subiendo archivos

---
+++ operadores.js
@@ -0,0 +1,55 @@
+//suma,reste,multiplicion y division
+//calcular valores de dos o mas elementos
+let num1 = 4;
+var num2 = 8;
+let suma = num1+num2;
+console.log("el resultado es:" + suma)
+//contadores
+let edad = 24
+edad=edad++;//edad++
+console.log("edad es:" + edad)
+//resta
+let rest1=9;
+let rest2=7;
+let resta =rest1-rest2;
+console.log("el ressultado es:" + resta)
+//contador resta
+let años = 17;
+años--;
+console.log( años)
+//multiplicacion
+let mult1=15;
+let mult2=2;
+let multiplicacion= mult1*mult2;
+console.log("el resultado es:" + multiplicacion)
+//divison
+let div1=26;
+let div2=5;
+let division= div1 / div2;
+console.log("el resutado es:" + division)
+//residuo
+let res1=26;
+let res2=5;
+let residuo=res1%res2;
+console.log("el residuo es:" + residuo)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ variables.js
@@ -0,0 +1,18 @@
+//string
+var sena ="ADSO";
+console.log(sena);
+//numero
+let numeroEntero = 17;
+var numeroNegativo = -4;
+let numeroDecimal = 11.54;
+//Booleanos trye-false
+var estoyEstudiando = true;
+let estoyAburido = false
+// undefined
+var num;
+console.log(num);
+num=0;
+console.log(num);
+//null
+var sinNada = null;
+console.log(sinNada);
