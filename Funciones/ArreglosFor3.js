efb14f7aa19875f9ac03795b74a69fcd64630bc9
PoolGoez207 <poolgomez27@gmail.com>
2025-09-12 21:21:18 -0500

    quinto

---
+++ Funciones/ArreglosFor3.js
@@ -0,0 +1,14 @@
+function sumaArreglos()
+{
+let num=[3,7,9,4,2]
+let num1=[2,5,7,3,9]
+let resp=[]
+
+for(let i=0; i<num.length; i++){
+
+    resp[i]=num[i] + num1[i]
+
+}
+console.log(resp)
+}
+sumaArreglos()
---
+++ Funciones/ArreglosVocales.js
@@ -0,0 +1,18 @@
+function buscar(vocalB)
+{
+let vocales=["a","e","i","o","u"]
+
+for(let i=0; i<vocales.length; i++ ){
+   
+    if(vocalB == vocales[i]){
+        console.log("vocal encontrada: " + vocalB)
+    }
+     else(vocalB == vocales[i])
+     {
+        console.log("vocal no encontrada: " + vocalB)
+     }
+}
+}
+let a=prompt("digite vocal a buscar")
+    buscar("a")
+    
